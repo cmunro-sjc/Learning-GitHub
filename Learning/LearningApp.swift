83f634a34183b2986ba66f3b7c0accb04e29140f
Caleb Munro <t.calebmunro@gmail.com>
2021-03-13 11:33:54 -0700

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Timothy Munro on 3/13/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
